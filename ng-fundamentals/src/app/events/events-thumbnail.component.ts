31c26e193b1899064cca6138cf6be581b553e102
Daniel Castro <44201218+DanielCastro1021@users.noreply.github.com>
2022-07-21 16:53:41 +0100

    Step 3 - Styling Components

--- ng-fundamentals/src/app/events/events-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/events-thumbnail.component.ts
@@ -3,10 +3,19 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'event-thumbnail',
   templateUrl: 'events-thumbnail.component.html',
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+    `,
+  ],
 })
 export class EventsThumbnailComponent {
   @Input() event: any;
-  someProperty: String = 'Child Name';
 
   logFoo(): void {
     console.log('foo');
